7b20273a3ca01e52bc4675586deaa82ce9794c7d
dksqudgus86@gmail.com <dksqudgus86@gmail.com>
2019-11-20 14:56:21 +0900

    초기 커밋.

---
+++ Project12/소스.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	char star = '*';
+	char* pstar = &star;
+
+	printf("주소: %d 코드값: %d 문자: %c\n", &pstar, *pstar, *pstar);
+
+	return 0;
+}
